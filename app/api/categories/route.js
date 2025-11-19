82b8aee39465069047c5666d5b4d35a7461ed275
Kyliejarvis121 <kyliejarvis121@gmail.com>
2025-11-19 20:30:15 +0100

    modify lib and routes

--- app/api/categories/route.js
+++ app/api/categories/route.js
@@ -3,55 +3,53 @@ import { NextResponse } from "next/server";
 
 export async function POST(request) {
   try {
-    const { title, slug, imageUrl, description, isActive } =
-      await request.json();
+    const { title, slug, imageUrl, description, isActive } = await request.json();
 
-    const existingCategory = await db.category.findUnique({
-      where: {
-        slug,
-      },
+    const existingCategory = await db.categories.findUnique({
+      where: { slug },
     });
+
     if (existingCategory) {
       return NextResponse.json(
         {
           data: null,
-          message: `Category ( ${title})  already exists in the Database`,
+          message: `Category (${title}) already exists`,
         },
         { status: 409 }
       );
     }
-    const newCategory = await db.category.create({
+
+    const newCategory = await db.categories.create({
       data: { title, slug, imageUrl, description, isActive },
     });
+
     return NextResponse.json(newCategory);
   } catch (error) {
     console.log(error);
     return NextResponse.json(
       {
-        message: "Failed to create Category",
-        error,
+        message: "Failed to create category",
+        error: error.message,
       },
       { status: 500 }
     );
   }
 }
-export async function GET(request) {
+
+
+export async function GET() {
   try {
-    const categories = await db.category.findMany({
-      orderBy: {
-        createdAt: "desc",
-      },
-      include: {
-        products: true,
-      },
+    const categories = await db.categories.findMany({
+      orderBy: { id: "desc" },
     });
+
     return NextResponse.json(categories);
   } catch (error) {
     console.log(error);
     return NextResponse.json(
       {
-        message: "Failed to Fetch Category",
-        error,
+        message: "Failed to fetch categories",
+        error: error.message,
       },
       { status: 500 }
     );
