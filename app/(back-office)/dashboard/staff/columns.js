d3b3c42d25e5400aec9b7be6630ab0c677ddfec0
Kyliejarvis121 <kyliejarvis121@gmail.com>
2025-11-20 16:58:47 +0100

    added culumns

---
+++ app/(back-office)/dashboard/staff/columns.js
@@ -0,0 +1,21 @@
+// app/(back-office)/dashboard/staff/columns.js
+
+export const columns = [
+    {
+      header: "Name",
+      accessorKey: "name",
+    },
+    {
+      header: "Email",
+      accessorKey: "email",
+    },
+    {
+      header: "Role",
+      accessorKey: "role",
+    },
+    {
+      header: "Actions",
+      accessorKey: "actions",
+    },
+  ];
+  
