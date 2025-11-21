41b062947bfee2f57bd09e94f5778a42d7b001d3
Kyliejarvis121 <kyliejarvis121@gmail.com>
2025-11-21 14:41:32 +0100

    db connection

--- app/(back-office)/dashboard/customers/update/[id]/page.jsx
+++ app/(back-office)/dashboard/customers/update/[id]/page.jsx
@@ -1,11 +1,19 @@
 import CustomerForm from "@/components/backoffice/CustomerForm";
 import FormHeader from "@/components/backoffice/FormHeader";
 import { getData } from "@/lib/getData";
-
 import React from "react";
 
 export default async function UpdateCustomer({ params: { id } }) {
-  const user = await getData(`users/${id}`);
+  const { success, data: user, error } = await getData(`users/${id}`);
+
+  if (!success) {
+    return (
+      <div className="text-red-600 p-4">
+        Failed to fetch customer: {error}
+      </div>
+    );
+  }
+
   return (
     <div>
       <FormHeader title="Update Customer" />
