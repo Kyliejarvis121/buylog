989b4ebf38e33d1a266a8ee3e6df9ad99879f23a
Kyliejarvis121 <kyliejarvis121@gmail.com>
2026-02-17 23:37:50 +0100

    updated

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { prisma } from "@/lib/prisma";
+import { prisma } from "@/lib/prismadb";
 import Pusher from "pusher";
 
 export async function POST(req) {
