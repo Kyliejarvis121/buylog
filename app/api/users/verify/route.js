23b1d6d35a9455959984a2bba1d12ebf23f4594f
Kyliejarvis121 <kyliejarvis121@gmail.com>
2026-02-24 16:44:07 +0100

    updated

--- app/api/users/verify/route.js
+++ app/api/users/verify/route.js
@@ -21,7 +21,7 @@ export async function GET(req) {
   await prisma.user.update({
     where: { id: user.id },
     data: {
-      emailVerified: true,
+      emailVerified: new Date(),
       emailVerificationToken: null,
     },
   });
