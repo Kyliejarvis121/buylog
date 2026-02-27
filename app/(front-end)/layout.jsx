412952fded89b5257491f663564540ca4af6f4ae
Kyliejarvis121 <kyliejarvis121@gmail.com>
2026-02-27 14:52:45 +0100

    updated

--- app/(front-end)/layout.jsx
+++ app/(front-end)/layout.jsx
@@ -5,23 +5,32 @@ import React from "react";
 
 export default function Layout({ children }) {
   return (
-    <div className="w-full overflow-x-hidden">
-      {/* AD SENSE SCRIPT (loads once) */}
-      <Script
-        async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9597681757893442"
-        crossOrigin="anonymous"
-        strategy="afterInteractive"
-      />
+    <html>
+      <head>
+        <meta
+          name="google-adsense-account"
+          content="ca-pub-9597681757893442"
+        />
 
-      <Navbar />
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9597681757893442"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+      </head>
 
-      {/* MAIN CONTENT */}
-      <main className="max-w-6xl mx-auto w-full px-4 sm:px-6 lg:px-0 py-6">
-        {children}
-      </main>
+      <body>
+        <div className="w-full overflow-x-hidden">
+          <Navbar />
 
-      <Footer />
-    </div>
+          <main className="max-w-6xl mx-auto w-full px-4 sm:px-6 lg:px-0 py-6">
+            {children}
+          </main>
+
+          <Footer />
+        </div>
+      </body>
+    </html>
   );
 }
