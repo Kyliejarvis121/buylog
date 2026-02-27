8c7125230fb254017186c7c621c8323f1392fbb3
Kyliejarvis121 <kyliejarvis121@gmail.com>
2026-02-27 14:35:44 +0100

    updated

--- app/(front-end)/layout.jsx
+++ app/(front-end)/layout.jsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import Footer from "@/components/frontend/Footer";
 import Navbar from "@/components/frontend/Navbar";
 import React from "react";
@@ -5,6 +6,14 @@ import React from "react";
 export default function Layout({ children }) {
   return (
     <div className="w-full overflow-x-hidden">
+      {/* AD SENSE SCRIPT (loads once) */}
+      <Script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9597681757893442"
+        crossOrigin="anonymous"
+        strategy="afterInteractive"
+      />
+
       <Navbar />
 
       {/* MAIN CONTENT */}
