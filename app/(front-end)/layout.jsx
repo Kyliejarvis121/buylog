0686e3e38d65b3633bf8c29548f139b872c9e9ad
Kyliejarvis121 <kyliejarvis121@gmail.com>
2026-03-04 00:53:02 +0100

    updated

--- app/(front-end)/layout.jsx
+++ app/(front-end)/layout.jsx
@@ -1,28 +1,21 @@
-import Script from "next/script";
 import Footer from "@/components/frontend/Footer";
 import Navbar from "@/components/frontend/Navbar";
 import ThemeProvider from "@/components/ThemeProvider";
 
-export default function RootLayout({ children }) {
+export default function Layout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body>
         <ThemeProvider>
           <div className="w-full overflow-x-hidden">
             <Navbar />
+
             <main className="max-w-6xl mx-auto w-full px-4 sm:px-6 lg:px-0 py-6">
               {children}
             </main>
+
             <Footer />
           </div>
-
-          {/* Google Ads Script (safe location) */}
-          <Script
-            async
-            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9597681757893442"
-            crossOrigin="anonymous"
-            strategy="afterInteractive"
-          />
         </ThemeProvider>
       </body>
     </html>
