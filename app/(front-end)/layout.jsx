5db6c9e8af41e76c03f9ed7d6b5f9472cd599384
Kyliejarvis121 <kyliejarvis121@gmail.com>
2026-03-04 12:42:10 +0100

    updated

--- app/(front-end)/layout.jsx
+++ app/(front-end)/layout.jsx
@@ -1,5 +1,7 @@
 import "@/styles/main.scss";
 import Providers from "@/context/Providers";
+import Navbar from "@/components/frontend/Navbar";
+import Footer from "@/components/frontend/Footer";
 import AdsenseScript from "@/components/frontend/AdsenseScript";
 
 export default function RootLayout({ children }) {
@@ -7,10 +9,18 @@ export default function RootLayout({ children }) {
     <html lang="en" className="dark" suppressHydrationWarning>
       <body>
         <Providers>
-          {/* Adsense loads after hydration */}
+          {/* Adsense loads safely after hydration */}
           <AdsenseScript />
 
-          {children}
+          <div className="w-full overflow-x-hidden">
+            <Navbar />
+
+            <main className="max-w-6xl mx-auto w-full px-4 sm:px-6 lg:px-0 py-6">
+              {children}
+            </main>
+
+            <Footer />
+          </div>
         </Providers>
       </body>
     </html>
