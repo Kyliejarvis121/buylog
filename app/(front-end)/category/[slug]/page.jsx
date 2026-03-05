482713b63477d4b9299716708c91f113a5897374
Kyliejarvis121 <kyliejarvis121@gmail.com>
2026-03-05 12:36:57 +0100

    updated

--- app/(front-end)/category/[slug]/page.jsx
+++ app/(front-end)/category/[slug]/page.jsx
@@ -1,4 +1,4 @@
-iimport FilterComponent from "@/components/frontend/Filter/FilterComponent";
+import FilterComponent from "@/components/frontend/Filter/FilterComponent";
 import { getData } from "@/lib/getData";
 import React from "react";
 
