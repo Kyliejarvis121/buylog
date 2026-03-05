8704f02b7cef106c6d1d6a2aac4daa8ddb36b25c
Kyliejarvis121 <kyliejarvis121@gmail.com>
2026-03-05 11:00:45 +0100

    updated

--- app/(back-office)/layout.js
+++ app/(back-office)/layout.js
@@ -13,7 +13,7 @@ export default function Layout({ children }) {
       {/* Sidebar */}
       <Sidebar showSidebar={showSidebar} setShowSidebar={setShowSidebar} />
 
-      {/* Main Content Area */}
+      {/* Main Content */}
       <div className="flex flex-col flex-1 lg:ml-64 bg-slate-100 dark:bg-slate-900">
 
         {/* Dashboard Header */}
@@ -21,7 +21,7 @@ export default function Layout({ children }) {
 
         {/* Page Content */}
         <main className="flex-1 mt-16 overflow-y-auto">
-          <div className="max-w-6xl mx-auto p-6 text-slate-900 dark:text-slate-100">
+          <div className="w-full px-6">
             {children}
           </div>
         </main>
