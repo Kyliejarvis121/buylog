c2a1620e0c56f67246807d6004e77c29cd8b88b8
Kyliejarvis121 <kyliejarvis121@gmail.com>
2026-03-05 11:10:46 +0100

    updated updated

--- app/(back-office)/layout.js
+++ app/(back-office)/layout.js
@@ -10,20 +10,15 @@ export default function Layout({ children }) {
   return (
     <div className="flex h-screen overflow-hidden">
 
-      {/* Sidebar */}
       <Sidebar showSidebar={showSidebar} setShowSidebar={setShowSidebar} />
 
-      {/* Main Content */}
       <div className="flex flex-col flex-1 lg:ml-64 bg-slate-100 dark:bg-slate-900">
 
-        {/* Dashboard Header */}
         <Navbar showSidebar={showSidebar} setShowSidebar={setShowSidebar} />
 
-        {/* Page Content */}
+        {/* Page Content (NO extra container) */}
         <main className="flex-1 mt-16 overflow-y-auto">
-          <div className="w-full px-6">
-            {children}
-          </div>
+          {children}
         </main>
 
       </div>
