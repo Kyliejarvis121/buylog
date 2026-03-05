4af7f4ad6aebbffb42cce020e0abc3cd9bde27dd
Kyliejarvis121 <kyliejarvis121@gmail.com>
2026-03-05 15:21:44 +0100

    updated

--- app/(back-office)/layout.js
+++ app/(back-office)/layout.js
@@ -8,16 +8,19 @@ export default function Layout({ children }) {
   const [showSidebar, setShowSidebar] = useState(false);
 
   return (
-    <div className="flex h-screen overflow-hidden">
+    <div className="flex min-h-screen">
 
+      {/* Sidebar */}
       <Sidebar showSidebar={showSidebar} setShowSidebar={setShowSidebar} />
 
+      {/* Main Content */}
       <div className="flex flex-col flex-1 lg:ml-64 bg-slate-100 dark:bg-slate-900">
 
+        {/* Navbar */}
         <Navbar showSidebar={showSidebar} setShowSidebar={setShowSidebar} />
 
-        {/* Page Content (NO extra container) */}
-        <main className="flex-1 mt-16 overflow-y-auto">
+        {/* Page Content */}
+        <main className="flex-1 mt-16 p-6 overflow-x-auto">
           {children}
         </main>
 
